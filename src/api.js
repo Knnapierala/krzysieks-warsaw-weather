323389952a0bfd24281b54a12fd6b3d828ed55c1
Knnapierala <knnapierala@wp.pl>
2022-08-25 22:57:37 +0200

    removed some comments

--- src/api.js
+++ src/api.js
@@ -2,5 +2,3 @@ export const warsawLon = "21.0118";
 export const warsawLat = "52.2298";
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-
-// `https://api.openweathermap.org/data/2.5/weather?lat=52.2298&lon=21.0118&appid=fe83c7453eb44edb5c308c75b9ae0eae`;
