58d93577af14aed9f90b6781e68a87a98c4e343f
Knnapierala <knnapierala@wp.pl>
2022-08-28 14:52:14 +0200

    added new elements to weather component and styled it

--- src/App.js
+++ src/App.js
@@ -4,23 +4,36 @@ import { WEATHER_API_URL, warsawLat, warsawLon } from "./api";
 import Weather from "./components/Weather/Weather";
 
 function App() {
+  const [isLoading, setIsLoading] = useState(true);
   const [warsawWeather, setWarsawWeather] = useState({});
   const date = new Date();
   console.log(warsawWeather);
-  const { name, main, weather } = warsawWeather;
+  const { name, main, weather, wind } = warsawWeather;
 
   useEffect(() => {
     fetch(
       `${WEATHER_API_URL}/weather?lat=${warsawLat}&lon=${warsawLon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
     )
       .then((response) => response.json())
-      .then((data) => setWarsawWeather(data));
+      .then((data) => setWarsawWeather(data))
+      .then(() => setIsLoading(false));
   }, []);
+  // useEffect(async () => {
+  //   const response = await fetch(
+  //     `${WEATHER_API_URL}/weather?lat=${warsawLat}&lon=${warsawLon}&appid=${process.env.REACT_APP_WEATHER_API_KEY}&units=metric`
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  //   setWarsawWeather(data);
+  // }, []);
+  if (isLoading) {
+    return <h1 className="loading">Loading...</h1>;
+  }
   return (
     <main className="container">
       <h1 className="main-title">{name}</h1>
       <h2>Today is: {date.toString().slice(0, 15)}</h2>
-      <Weather weather={weather} main={main} />
+      <Weather weather={weather} main={main} wind={wind} />
     </main>
   );
 }
--- src/components/Weather/Weather.js
+++ src/components/Weather/Weather.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./weather.css";
 
-const Weather = ({ main, weather }) => {
+const Weather = ({ main, weather, wind }) => {
   const { temp } = main;
   const iconID = weather[0].icon;
   const iconLink = `http://openweathermap.org/img/wn/${iconID}@2x.png`;
@@ -15,7 +15,21 @@ const Weather = ({ main, weather }) => {
           <p>{weather[0].description}</p>
         </div>
       </div>
-      <div className="section-bottom"></div>
+      <div className="section-bottom">
+        <h1>More infos:</h1>
+        <div className="info">
+          <div className="info-name">Humidity:</div>
+          <div className="info-value">{main.humidity} %</div>
+        </div>
+        <div className="info">
+          <div className="info-name">Pressure:</div>
+          <div className="info-value">{main.pressure} hPa</div>
+        </div>
+        <div className="info">
+          <div className="info-name">Wind:</div>
+          <div className="info-value">{wind.speed} m/s</div>
+        </div>
+      </div>
     </section>
   );
 };
